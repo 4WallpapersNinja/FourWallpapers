5e350cf4a0e9d99e4d8eae1aebf4aa6c03404b97
The Architect <me@thearchitect.io>
2017-09-28 20:32:19 -0400

    more clean up

--- FourWallpapers.Core/Database/Entities/UserClaims.cs
+++ FourWallpapers.Core/Database/Entities/UserClaims.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace FourWallpapers.Core.Database.Entities
 {
     public class UserClaim : BaseEntity
--- FourWallpapers.Providers.TwoFactorAuthentication/TwoFactorAuthenticator.cs
+++ FourWallpapers.Providers.TwoFactorAuthentication/TwoFactorAuthenticator.cs
@@ -1,31 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Numerics;
 using System.Security.Cryptography;
 using System.Text;
-using ImageSharp;
+using ZXing;
 using ZXing.QrCode;
-using SixLabors.ImageSharp.PixelFormats;
+// ReSharper disable ConditionIsAlwaysTrueOrFalse
 
 namespace FourWallpapers.Providers.TwoFactorAuthentication
 {
     /// <summary>
-    /// Initially Copied from https://github.com/andyedinborough/GoogleAuthenticator/tree/dotnetcore
+    ///     Initially Copied from https://github.com/andyedinborough/GoogleAuthenticator/tree/dotnetcore
     /// </summary>
     public class TwoFactorAuthenticator
     {
         public static DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        public TimeSpan DefaultClockDriftTolerance { get; set; }
 
         public TwoFactorAuthenticator()
         {
             DefaultClockDriftTolerance = TimeSpan.FromMinutes(5);
         }
 
+        public TimeSpan DefaultClockDriftTolerance { get; set; }
+
         /// <summary>
-        /// Generate a setup code for a Google Authenticator user to scan (with issuer ID).
+        ///     Generate a setup code for a Google Authenticator user to scan (with issuer ID).
         /// </summary>
         /// <param name="issuer">Issuer ID (the name of the system, i.e. 'MyApp')</param>
         /// <param name="accountTitleNoSpaces">Account Title (no spaces)</param>
@@ -33,20 +32,23 @@ namespace FourWallpapers.Providers.TwoFactorAuthentication
         /// <returns>SetupCode object</returns>
         public SetupCode GenerateSetupCode(string issuer, string accountTitleNoSpaces, string accountSecretKey)
         {
-            if (string.IsNullOrWhiteSpace(accountTitleNoSpaces)) { throw new NullReferenceException("Account Title is null"); }
+            if (string.IsNullOrWhiteSpace(accountTitleNoSpaces))
+                throw new NullReferenceException("Account Title is null");
 
             accountTitleNoSpaces = accountTitleNoSpaces.Replace(" ", "");
 
-            SetupCode sC = new SetupCode
+            var sC = new SetupCode
             {
                 Account = accountTitleNoSpaces,
                 AccountSecretKey = accountSecretKey,
                 ManualEntryKey = EncodeAccountSecretKey(accountSecretKey)
             };
 
-            string provisionUrl = string.IsNullOrEmpty(issuer) ? $"otpauth://totp/{accountTitleNoSpaces}?secret={sC.ManualEntryKey}" : $"otpauth://totp/{accountTitleNoSpaces}?secret={ sC.ManualEntryKey}&issuer={UrlEncode(issuer)}";
+            var provisionUrl = string.IsNullOrEmpty(issuer)
+                ? $"otpauth://totp/{accountTitleNoSpaces}?secret={sC.ManualEntryKey}"
+                : $"otpauth://totp/{accountTitleNoSpaces}?secret={sC.ManualEntryKey}&issuer={UrlEncode(issuer)}";
 
-            string url = GenerateQrImageString(provisionUrl);
+            var url = GenerateQrImageString(provisionUrl);
 
             sC.QrCodeSetupImageUrl = url;
 
@@ -55,9 +57,9 @@ namespace FourWallpapers.Providers.TwoFactorAuthentication
 
         private string GenerateQrImageString(string qrCodeContent)
         {
-            var qrCodeWriter = new ZXing.BarcodeWriterSvg()
+            var qrCodeWriter = new BarcodeWriterSvg
             {
-                Format = ZXing.BarcodeFormat.QR_CODE,
+                Format = BarcodeFormat.QR_CODE,
                 Options = new QrCodeEncodingOptions
                 {
                     Height = 250,
@@ -71,20 +73,14 @@ namespace FourWallpapers.Providers.TwoFactorAuthentication
 
         private static string UrlEncode(string value)
         {
-            StringBuilder result = new StringBuilder();
+            var result = new StringBuilder();
             const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~";
 
-            foreach (char symbol in value)
-            {
+            foreach (var symbol in value)
                 if (validChars.IndexOf(symbol) != -1)
-                {
                     result.Append(symbol);
-                }
                 else
-                {
-                    result.Append('%' + string.Format("{0:X2}", (int)symbol));
-                }
-            }
+                    result.Append('%' + string.Format("{0:X2}", (int) symbol));
 
             return result.ToString().Replace(" ", "%20");
         }
@@ -98,22 +94,22 @@ namespace FourWallpapers.Providers.TwoFactorAuthentication
         {
             const int inByteSize = 8;
             const int outByteSize = 5;
-            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".ToCharArray();
+            var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".ToCharArray();
 
             int i = 0, index = 0;
-            StringBuilder result = new StringBuilder((data.Count + 7) * inByteSize / outByteSize);
+            var result = new StringBuilder((data.Count + 7) * inByteSize / outByteSize);
 
             while (i < data.Count)
             {
-                var currentByte = (data[i] >= 0) ? data[i] : (data[i] + 256);
+                var currentByte = data[i] >= 0 ? data[i] : data[i] + 256;
 
                 /* Is the current digit going to span a byte boundary? */
-                var digit = 0;
-                if (index > (inByteSize - outByteSize))
+                int digit;
+                if (index > inByteSize - outByteSize)
                 {
                     int nextByte;
-                    if ((i + 1) < data.Count)
-                        nextByte = (data[i + 1] >= 0) ? data[i + 1] : (data[i + 1] + 256);
+                    if (i + 1 < data.Count)
+                        nextByte = data[i + 1] >= 0 ? data[i + 1] : data[i + 1] + 256;
                     else
                         nextByte = 0;
 
@@ -143,33 +139,31 @@ namespace FourWallpapers.Providers.TwoFactorAuthentication
 
         internal string GenerateHashedCode(string secret, long iterationNumber, int digits = 6)
         {
-            byte[] key = Encoding.UTF8.GetBytes(secret);
+            var key = Encoding.UTF8.GetBytes(secret);
             return GenerateHashedCode(key, iterationNumber, digits);
         }
 
         internal string GenerateHashedCode(byte[] key, long iterationNumber, int digits = 6)
         {
-            byte[] counter = BitConverter.GetBytes(iterationNumber);
+            var counter = BitConverter.GetBytes(iterationNumber);
 
             if (BitConverter.IsLittleEndian)
-            {
                 Array.Reverse(counter);
-            }
 
-            HMACSHA1 hmac = new HMACSHA1(key);
+            var hmac = new HMACSHA1(key);
 
-            byte[] hash = hmac.ComputeHash(counter);
+            var hash = hmac.ComputeHash(counter);
 
-            int offset = hash[hash.Length - 1] & 0xf;
+            var offset = hash[hash.Length - 1] & 0xf;
 
             // Convert the 4 bytes into an integer, ignoring the sign.
-            int binary =
+            var binary =
                 ((hash[offset] & 0x7f) << 24)
                 | (hash[offset + 1] << 16)
                 | (hash[offset + 2] << 8)
-                | (hash[offset + 3]);
+                | hash[offset + 3];
 
-            int password = binary % (int)Math.Pow(10, digits);
+            var password = binary % (int) Math.Pow(10, digits);
             return password.ToString(new string('0', digits));
         }
 
@@ -180,7 +174,7 @@ namespace FourWallpapers.Providers.TwoFactorAuthentication
 
         private long GetCurrentCounter(DateTime now, DateTime epoch, int timeStep)
         {
-            return (long)(now - epoch).TotalSeconds / timeStep;
+            return (long) (now - epoch).TotalSeconds / timeStep;
         }
 
 
@@ -189,7 +183,8 @@ namespace FourWallpapers.Providers.TwoFactorAuthentication
             return ValidateTwoFactorPin(accountSecretKey, twoFactorCodeFromClient, DefaultClockDriftTolerance);
         }
 
-        public bool ValidateTwoFactorPin(string accountSecretKey, string twoFactorCodeFromClient, TimeSpan timeTolerance)
+        public bool ValidateTwoFactorPin(string accountSecretKey, string twoFactorCodeFromClient,
+            TimeSpan timeTolerance)
         {
             var codes = GetCurrentPiNs(accountSecretKey, timeTolerance);
             return codes.Any(c => c == twoFactorCodeFromClient);
@@ -212,22 +207,18 @@ namespace FourWallpapers.Providers.TwoFactorAuthentication
 
         public string[] GetCurrentPiNs(string accountSecretKey, TimeSpan timeTolerance)
         {
-            List<string> codes = new List<string>();
-            long iterationCounter = GetCurrentCounter();
-            int iterationOffset = 0;
+            var codes = new List<string>();
+            var iterationCounter = GetCurrentCounter();
+            var iterationOffset = 0;
 
             if (timeTolerance.TotalSeconds > 30)
-            {
                 iterationOffset = Convert.ToInt32(timeTolerance.TotalSeconds / 30.00);
-            }
 
-            long iterationStart = iterationCounter - iterationOffset;
-            long iterationEnd = iterationCounter + iterationOffset;
+            var iterationStart = iterationCounter - iterationOffset;
+            var iterationEnd = iterationCounter + iterationOffset;
 
-            for (long counter = iterationStart; counter <= iterationEnd; counter++)
-            {
+            for (var counter = iterationStart; counter <= iterationEnd; counter++)
                 codes.Add(GeneratePinAtInterval(accountSecretKey, counter));
-            }
 
             return codes.ToArray();
         }
